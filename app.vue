7d7b102570482442d61cae81eff7a04bda54a48f
Baldor <james@baldor.dev>
2025-01-14 18:28:34 +0100

    project creation/modules setup

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+<script setup lang="ts">
+const runtimeConfig = useRuntimeConfig();
+console.log(runtimeConfig.public.directus)
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,32 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/devtools',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/fonts',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/i18n',
+    'nuxt-lucide-icons',
+    'nuxt-directus'
+  ],
+  image: {
+    // Options
+  },
+  fonts: {
+    // Options
+  },
+  tailwindcss: {
+    // Options
+  },
+  runtimeConfig: {
+    public: {
+      directus: {
+        url: process.env.DIRECTUS_URL
+      },
+
+    }
+  }
+})
