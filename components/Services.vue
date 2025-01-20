2c68ead6621b41475cc269ba0186860080a33b6b
Baldor <james@baldor.dev>
2025-01-20 18:00:41 +0100

    services component added

---
+++ components/Services.vue
@@ -0,0 +1,26 @@
+<template>
+    <section id="services" class="py-20 bg-gray-100">
+        <div class="max-w-6xl mx-auto px-4">
+            <h2 class="text-4xl font-bold text-center mb-16 text-gray-700">Services & Pricing</h2>
+            <div class="grid md:grid-cols-3 gap-8">
+                {{ fetchCollections() }}
+            </div>
+        </div>
+    </section>
+</template>
+<script setup lang="ts">
+
+interface Services {
+    id: number;
+    title: string;
+    description: string;
+    price: string;
+    icon: string;
+    icon_color: string;
+    bg_color: string;
+}
+const { getCollections } = useDirectusCollections();
+const router = useRouter();
+const fetchCollections = async () => {  const collections = await getCollections();};
+
+</script>
