3ab35f172cd01a06f7284a40146aac77210c1177
Baldor <james@baldor.dev>
2025-01-20 19:53:46 +0100

    Navbar styling

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,13 +1,9 @@
 <template>
-    <nav class="fixed top-0 w-full bg-white/90 background-blur-sm shadow-sm z-50">
-        <div class="max-auto px-4 py-4 flex justify-between items-center">
+    <nav class="fixed pr-3 top-0 w-full bg-white/90 background-blur-sm shadow-sm z-50  flex justify-between">
+        <div class="max-auto px-4 py-4 sm:flex-col flex justify-between items-center">
             <span class="text-xl font-bold text-gray-700">
                 Web Solutions for Businesses
             </span>
-            <button class="lg:hidden text-gray-600 hover:text-orange-400 transition-colors" @click="isOpen = !isOpen"
-                aria-label="Toggle menu">
-                <Icon :name="isOpen ? 'heroicons:x-mark' : 'heroicons:bars-3'" class="w-6 h-6" />
-            </button>
             <div class="hidden lg:flex space-x-8 flex-wrap items-center justify-center gap-2">
                 <NuxtLink to="#projects" class="text-gray-600 hover:text-orange-400 transition-colors">Results
                 </NuxtLink>
@@ -17,42 +13,35 @@
                     class="bg-orange-400 text-white px-4 py-2 rounded-lg hover:bg-orange-500 transition-colors">Get
                     Qhote</NuxtLink>
             </div>
-                <Transition
-        enter-active-class="transition duration-200 ease-out"
-        enter-from-class="transform scale-95 opacity-0"
-        enter-to-class="transform scale-100 opacity-100"
-        leave-active-class="transition duration-100 ease-in"
-        leave-from-class="transform scale-100 opacity-100"
-        leave-to-class="transform scale-95 opacity-0"
-      >
-        <div
-          v-if="isOpen"
-          class="lg:hidden mt-4 pb-4 space-y-4 flex flex-col items-start"
-        >
-          <NuxtLink 
-            to="#projects" 
-            class="text-gray-600 hover:text-orange-400 transition-colors"
-            @click="isOpen = false"
-          >
-            Results
-          </NuxtLink>
-          <NuxtLink 
-            to="#services" 
-            class="text-gray-600 hover:text-orange-400 transition-colors"
-            @click="isOpen = false"
-          >
-            Services
-          </NuxtLink>
-          <NuxtLink 
-            to="#contact" 
-            class="bg-orange-400 text-white px-4 py-2 rounded-lg hover:bg-orange-500 transition-colors"
-            @click="isOpen = false"
-          >
-            Get Quote
-          </NuxtLink>
+            <Transition enter-active-class="transition duration-200 ease-out"
+                enter-from-class="transform scale-95 opacity-0" enter-to-class="transform scale-100 opacity-100"
+                leave-active-class="transition duration-100 ease-in" leave-from-class="transform scale-100 opacity-100"
+                leave-to-class="transform scale-95 opacity-0">
+                <div v-if="isOpen" class="lg:hidden mt-4 pb-4 space-y-4 flex flex-col items-start">
+                    <div class="flex flex-col items-center justify-center h-full space-y-8">
+                        <NuxtLink to="#projects" class="text-gray-600 hover:text-orange-400 transition-colors"
+                            @click="isOpen = false">
+                            Results
+                        </NuxtLink>
+                        <NuxtLink to="#services" class="text-gray-600 hover:text-orange-400 transition-colors"
+                            @click="isOpen = false">
+                            Services
+                        </NuxtLink>
+                        <NuxtLink to="#contact"
+                            class="bg-orange-400 text-white px-4 py-2 rounded-lg hover:bg-orange-500 transition-colors"
+                            @click="isOpen = false">
+                            Get Quote
+                        </NuxtLink>
+
+                    </div>
+                </div>
+            </Transition>
         </div>
-      </Transition>
-        </div>
+        <button class="lg:hidden text-gray-600 hover:text-orange-400 transition-colors" @click="isOpen = !isOpen"
+            aria-label="Toggle menu">
+            <Icon :name="isOpen ? 'heroicons:x-mark' : 'heroicons:bars-3'" class="w-6 h-6" />
+        </button>
+
     </nav>
 </template>
 <script setup lang="ts">
