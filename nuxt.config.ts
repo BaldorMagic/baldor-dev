918aa7171257697289f77b67d3ade9f75c0d4b21
Baldor <james@baldor.dev>
2025-01-24 14:48:13 +0100

    open graph protocol added and imaged asset added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,12 @@ export default defineNuxtConfig({
       title: 'Creative Solutions Through Code | Frontend Developer',
       meta: [
         {name: 'description', content: 'Frontend developer specialized in Vue.js, tailwindcss and Typescript. Building performant, accessible and beautiful web applications.'},
-        {name: 'robots', content: 'index, follow'}
+        {name: '', content: 'index, follow'},
+        {property: 'og:title', content: 'Creative Solutions Through Code | Frontend Developer'},
+        {property: 'og:type', content: 'website'},
+        {property: 'og:url', content: 'https://baldor.dev'},
+        {property: 'og:image', content: '/og-image.webp'},
+        {property: 'og:image:alt', content: 'An alien cat walking on a planet'},
       ],
       htmlAttrs: {
         lang: 'en'
